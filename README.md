3850fc23bd702f7e5d60fc0e1e4fe6b16fb5fbde
Alexander Yang <liming.alexander@yahoo.com>
2014-09-25 11:29:41 -0700

    added Stories, a file where possible Story events will be

---
+++ README.md
@@ -0,0 +1,9 @@
+This is a personal game I was making. A place where I could live out my dreams
+and events in my life. My hope is that in the course of over a few decades
+it will be something that will culminate my experiences into a greater work of
+art. Hopefully, people will be able to play it and live through the things that
+I have lived through.
+
+Most of all though, it is a place where creativity has no bounds. I wish this
+project to allow myself a space where I can channel my energies and emotions
+and thoughts into a whole.
--- SG/Words.cs
+++ SG/Words.cs
@@ -6,6 +6,24 @@ using System.Threading.Tasks;
 
 namespace SG
 {
+
+    public class Synnies : Word
+    {
+        /// <summary>
+        /// A word to describe things that synergize together,
+        /// it's a concept that I came up with after dreaming
+        /// about trying to understand some quiz and having people
+        /// help me explain what Synnies was. I don't really understand
+        /// the idea of synnies, but I want to be able to incorporate it
+        /// as a game mechanic. It is not meant to be a Word, but it
+        /// is here for the meantime so I won't forget about it.
+        /// </summary>
+        public Synnies()
+            : base("Synnies")
+        {
+        }
+    }
+
     public class Strength : Word
     {
         public Strength()
@@ -21,6 +39,24 @@ namespace SG
         public override string Description()
         {
             return "Word Strength!";
+        }
+    }
+
+    public class Tears : Word
+    {
+        public Tears()
+            : base("Tears")
+        {
+        }
+
+        public override void Action()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override string Description()
+        {
+            return "Word Tears";
         }
     }
 }
